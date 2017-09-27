cc8679798ef61146537c30fa8ff2fa0606d5e1d7
kramstrom <kasper.ram@gmail.com>
2017-09-27 20:40:15 +0200

    game_platform.py docs

--- src/game_platform/game_platform.py
+++ src/game_platform/game_platform.py
@@ -2,7 +2,19 @@ from board import Board
 import random
 
 class Game:
+    """
+	Game is the main class for the Game Platform Interface
+	"""
     def __init__(self, ui, player1, player2):
+        """
+		Construct a new Game object.
+
+		:param self: A reference to the Game object itself
+		:param ui: The user interface for the Game object
+		:param player1: A reference to Player 1 in the current Game
+		:param player2: A reference to Player 2 in the current Game
+		:return: returns nothing
+		"""
         self.player1 = player1
         self.player2 = player2
 
@@ -18,9 +30,22 @@ class Game:
         self.displayBoard(ui)
 
     def displayBoard(self, ui):
+        """
+		Displays the Game's user interface
+
+		:param self: A reference to the Game object itself
+		:param self: The user interface to display
+		:return: returns nothing
+		"""
         self.board = Board(ui)
 
     def checkWin(self):
+        """
+		Checks if a Player in the Game reached winning conditions
+
+		:param self: A reference to the Game object itself
+		:return: returns nothing
+		"""
         winstates = [(0, 1, 2),
                      (3, 4, 5),
                      (6, 7, 8),
@@ -38,11 +63,25 @@ class Game:
 
 
     def makemove(self,boxId):
+        """
+		Performs a move for the current player.
+
+		:param self: A reference to the Game object itself
+		:param boxId: The index of where the move will be done in the game state list
+		:return: returns nothing
+		"""
         self.gameState[boxId] = self.currentplayer
         self.currentplayer = self.currentplayer * -1
         self.board.updateBoardState(self.gameState)
 
     def click(self, mousepos):
+        """
+		Handles mouse clicks within the Game coordinates
+
+		:param self: A reference to the GameUI object itself
+		:param mousepos: The coordinates of where the mouse was clicked
+		:return: returns a boolean whether current Player has won
+		"""
         boxId = self.board.findClickedBox(mousepos)
         if self.gameState[boxId] != 0:
             return
